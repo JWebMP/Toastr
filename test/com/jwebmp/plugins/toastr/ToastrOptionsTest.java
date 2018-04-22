3cf242c8cfb3f0ad73b19738ca56a8d569846305
GedMarc <ged_marc@hotmail.com>
2018-04-22 15:47:49 +0200

    Prepping for RC1 - Update Base Package Name to new Name

--- test/com/jwebmp/plugins/toastr/ToastrOptionsTest.java
+++ test/com/jwebmp/plugins/toastr/ToastrOptionsTest.java
@@ -17,7 +17,6 @@
 
 package com.jwebmp.plugins.toastr;
 
-import com.jwebmp.BaseTestClass;
 import com.jwebmp.plugins.easingeffects.JQEasingEffects;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -25,7 +24,7 @@ import org.junit.jupiter.api.Test;
 /**
  * @author Marc Magon
  */
-public class ToastrOptionsTest extends BaseTestClass
+public class ToastrOptionsTest
 {
 
 	public ToastrOptionsTest()
@@ -44,27 +43,16 @@ public class ToastrOptionsTest extends BaseTestClass
 	{
 		//ToastrFeature to = new ToastrFeature(ToastrType.Info, "message", null);
 		ToastrFeature to = new ToastrFeature("Message");
-		to.getOptions().setDebug(true);
-		to.getOptions().setShowMethod(JQEasingEffects.fadeIn);
+		to.getOptions()
+		  .setDebug(true);
+		to.getOptions()
+		  .setShowMethod(JQEasingEffects.fadeIn);
 		System.out.println(to.renderJavascript());
 
-		Assertions.assertEquals("toastr.options = {\n"
-				                    + "  \"closeButton\" : false,\n"
-				                    + "  \"debug\" : true,\n"
-				                    + "  \"newestOnTop\" : true,\n"
-				                    + "  \"progressBar\" : false,\n"
-				                    + "  \"positionClass\" : \"toast-top-right\",\n"
-				                    + "  \"preventDuplicates\" : false,\n"
-				                    + "  \"showDuration\" : \"300\",\n"
-				                    + "  \"hideDuration\" : \"1000\",\n"
-				                    + "  \"timeOut\" : \"5000\",\n"
-				                    + "  \"extendedTimeOut\" : \"1000\",\n"
-				                    + "  \"showEasing\" : \"swing\",\n"
-				                    + "  \"hideEasing\" : \"linear\",\n"
-				                    + "  \"showMethod\" : \"fadeIn\",\n"
-				                    + "  \"hideMethod\" : \"fadeOut\",\n"
-				                    + "  \"escapeHtml\" : false\n"
-				                    + "};toastr['info']('Message');", to.renderJavascript().toString());
+		Assertions.assertEquals(
+				"toastr.options = {\n" + "  \"closeButton\" : false,\n" + "  \"debug\" : true,\n" + "  \"newestOnTop\" : true,\n" + "  \"progressBar\" : false,\n" + "  \"positionClass\" : \"toast-top-right\",\n" + "  \"preventDuplicates\" : false,\n" + "  \"showDuration\" : \"300\",\n" + "  \"hideDuration\" : \"1000\",\n" + "  \"timeOut\" : \"5000\",\n" + "  \"extendedTimeOut\" : \"1000\",\n" + "  \"showEasing\" : \"swing\",\n" + "  \"hideEasing\" : \"linear\",\n" + "  \"showMethod\" : \"fadeIn\",\n" + "  \"hideMethod\" : \"fadeOut\",\n" + "  \"escapeHtml\" : false\n" + "};toastr['info']('Message');",
+				to.renderJavascript()
+				  .toString());
 	}
 
 	@Test
@@ -72,27 +60,16 @@ public class ToastrOptionsTest extends BaseTestClass
 	{
 		//ToastrFeature to = new ToastrFeature(ToastrType.Info, "message", null);
 		ToastrFeature to = new ToastrFeature(ToastrType.Error, "Title Test", "Message");
-		to.getOptions().setDebug(true);
-		to.getOptions().setShowMethod(JQEasingEffects.easeOutSine);
+		to.getOptions()
+		  .setDebug(true);
+		to.getOptions()
+		  .setShowMethod(JQEasingEffects.easeOutSine);
 		System.out.println(to.renderJavascript());
 
-		Assertions.assertEquals("toastr.options = {\n"
-				                    + "  \"closeButton\" : false,\n"
-				                    + "  \"debug\" : true,\n"
-				                    + "  \"newestOnTop\" : true,\n"
-				                    + "  \"progressBar\" : false,\n"
-				                    + "  \"positionClass\" : \"toast-top-right\",\n"
-				                    + "  \"preventDuplicates\" : false,\n"
-				                    + "  \"showDuration\" : \"300\",\n"
-				                    + "  \"hideDuration\" : \"1000\",\n"
-				                    + "  \"timeOut\" : \"5000\",\n"
-				                    + "  \"extendedTimeOut\" : \"1000\",\n"
-				                    + "  \"showEasing\" : \"swing\",\n"
-				                    + "  \"hideEasing\" : \"linear\",\n"
-				                    + "  \"showMethod\" : \"easeOutSine\",\n"
-				                    + "  \"hideMethod\" : \"fadeOut\",\n"
-				                    + "  \"escapeHtml\" : false\n"
-				                    + "};toastr['error']('Message','Title Test');", to.renderJavascript().toString());
+		Assertions.assertEquals(
+				"toastr.options = {\n" + "  \"closeButton\" : false,\n" + "  \"debug\" : true,\n" + "  \"newestOnTop\" : true,\n" + "  \"progressBar\" : false,\n" + "  \"positionClass\" : \"toast-top-right\",\n" + "  \"preventDuplicates\" : false,\n" + "  \"showDuration\" : \"300\",\n" + "  \"hideDuration\" : \"1000\",\n" + "  \"timeOut\" : \"5000\",\n" + "  \"extendedTimeOut\" : \"1000\",\n" + "  \"showEasing\" : \"swing\",\n" + "  \"hideEasing\" : \"linear\",\n" + "  \"showMethod\" : \"easeOutSine\",\n" + "  \"hideMethod\" : \"fadeOut\",\n" + "  \"escapeHtml\" : false\n" + "};toastr['error']('Message','Title Test');",
+				to.renderJavascript()
+				  .toString());
 	}
 
 	@Test
@@ -101,22 +78,9 @@ public class ToastrOptionsTest extends BaseTestClass
 		ToastrFeature to = new ToastrFeature();
 		System.out.println(to.renderJavascript());
 
-		Assertions.assertEquals("toastr.options = {\n"
-				                    + "  \"closeButton\" : false,\n"
-				                    + "  \"debug\" : false,\n"
-				                    + "  \"newestOnTop\" : true,\n"
-				                    + "  \"progressBar\" : false,\n"
-				                    + "  \"positionClass\" : \"toast-top-right\",\n"
-				                    + "  \"preventDuplicates\" : false,\n"
-				                    + "  \"showDuration\" : \"300\",\n"
-				                    + "  \"hideDuration\" : \"1000\",\n"
-				                    + "  \"timeOut\" : \"5000\",\n"
-				                    + "  \"extendedTimeOut\" : \"1000\",\n"
-				                    + "  \"showEasing\" : \"swing\",\n"
-				                    + "  \"hideEasing\" : \"linear\",\n"
-				                    + "  \"showMethod\" : \"fadeIn\",\n"
-				                    + "  \"hideMethod\" : \"fadeOut\",\n"
-				                    + "  \"escapeHtml\" : false\n"
-				                    + "};toastr['info']('');", to.renderJavascript().toString());
+		Assertions.assertEquals(
+				"toastr.options = {\n" + "  \"closeButton\" : false,\n" + "  \"debug\" : false,\n" + "  \"newestOnTop\" : true,\n" + "  \"progressBar\" : false,\n" + "  \"positionClass\" : \"toast-top-right\",\n" + "  \"preventDuplicates\" : false,\n" + "  \"showDuration\" : \"300\",\n" + "  \"hideDuration\" : \"1000\",\n" + "  \"timeOut\" : \"5000\",\n" + "  \"extendedTimeOut\" : \"1000\",\n" + "  \"showEasing\" : \"swing\",\n" + "  \"hideEasing\" : \"linear\",\n" + "  \"showMethod\" : \"fadeIn\",\n" + "  \"hideMethod\" : \"fadeOut\",\n" + "  \"escapeHtml\" : false\n" + "};toastr['info']('');",
+				to.renderJavascript()
+				  .toString());
 	}
 }
