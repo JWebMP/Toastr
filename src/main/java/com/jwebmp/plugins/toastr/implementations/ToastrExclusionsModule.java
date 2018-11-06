03eea871627e7a9919ad240bbaeeb65ef29ddde4
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:37 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/toastr/implementations/ToastrExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.toastr.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ToastrExclusionsModule
+		implements IGuiceScanModuleExclusions<ToastrExclusionsModule>,
+				           IGuiceScanJarExclusions<ToastrExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-toastr-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.toastr");
+		return strings;
+	}
+}
