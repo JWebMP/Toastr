0e8d53735754fcf98d371ee129930c6b2cc55fd9
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:02:58 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/toastr/ToastrPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/toastr/ToastrPageConfigurator.java
@@ -90,7 +90,7 @@ public class ToastrPageConfigurator
 
 	@NotNull
 	@Override
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
